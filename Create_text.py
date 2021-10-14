9ef0495a5d8cdcc3ffd494cf211d5808cd1d41f3
MosheOgalbo <https://github.com/MosheOgalbo>
2021-10-14 23:57:46 +0300

    first commitv

--- Create_text.py
+++ Create_text.py
@@ -1,4 +1,4 @@
 with open("\\Users\\moshe_ogalbo\\Jenkins-Projects-Final-1","w") as f:
-    f.write('hllo my name is moshe')
-    f.write("moshe loves devops")
-    f.write("moshe is your lecturer")
+    f.write('hllo my name is moshe\n')
+    f.write("moshe loves devops\n")
+    f.write("moshe is your lecturer\n")
