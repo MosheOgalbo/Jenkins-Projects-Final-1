af5a7486330a5a83ee5aefa8d887cac4b5146628
MosheOgalbo <https://github.com/MosheOgalbo>
2021-10-14 23:28:29 +0300

    first commitv

--- check_words.py
+++ check_words.py
@@ -1,6 +1,6 @@
 def word_count():
     count = 0
-    with open("\\Users\\moshe_ogalbo\\Jenkins-Projects-Final-1\\file.txt") as f:
+    with open("/Users/moshe_ogalbo/Jenkins-Projects-Final-1") as f:
         data = f.readLines()
         for line in data:
             words = line.split()
