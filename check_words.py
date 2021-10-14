306d6f8f38d35066503e31a478d029794c764380
MosheOgalbo <https://github.com/MosheOgalbo>
2021-10-15 01:57:41 +0300

    testcc

--- check_words.py
+++ check_words.py
@@ -1,11 +1,11 @@
 def word_count():
     count = 0
     with open("Users\\moshe_ogalbo\\Jenkins-Projects-Final-1\\file.txt") as f:
-        data = f.readLines()
+        data = f.readlines()
         for line in data:
             words = line.split()
             for word in words:
-                if word == "Alex":
+                if word == "moshe":
                     count += 1
     return count
 print(word_count())
